bc616873974b93f7eaec5df54450ace809168693
Tran Duc <ducck92@gmail.com>
2023-11-09 16:11:34 +0700

    first commit

---
+++ content-script.js
@@ -0,0 +1,40 @@
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+	console.log(sender.tab ? 'from a content script:' + sender.tab.url : 'from the extension')
+	console.log('ssssssssss', request)
+  if (request.type !== 'check_widget_code') {
+    sendResponse('received')
+  }
+
+  let $widget = document.querySelector('#wpwidget')
+  let isCodeInSource = findSubizScriptTag()
+
+  sendResponse({
+    subiz_code_in_source: isCodeInSource,
+    subiz_widget_loaded: $widget ? true: false,
+  })
+})
+
+console.log("content_scriptssssssss")
+
+function isEmbededCodeValid (str = '') {
+  return str.indexOf('!function(') > -1 && str.indexOf('s,u,b,i,z') > -1 && str.indexOf('setAccount') > -1 && str.indexOf('_subiz_init_') > -1
+}
+
+function findSubizScriptTag() {
+  let $scripts = document.querySelectorAll('script')
+  let result = false
+  let hasTimeout = false
+  for (let i = 0; i < $scripts.length; i++) {
+    let $script = $scripts[i]
+    let text = $script.innerHTML || ''
+    console.log("script texttttt", text)
+    if (isEmbededCodeValid(text)) {
+      result = true
+      let textTrim = (text || '').trim()
+      hasTimeout = textTrim.startsWith('setTimeout')
+      break
+    }
+  }
+
+  return result
+}
---
+++ script.js
@@ -0,0 +1,10 @@
+console.log('script.jssssssssssssss')
+setTimeout(function () {
+	/* Example: Send data from the page to your Chrome extension */
+	document.dispatchEvent(
+		new CustomEvent('SBZ_2023', {
+			detail: window.sbz_store, // Some variable from Gmail.
+		}),
+	)
+	console.log('111111111111', window.sbz_store)
+}, 0)
